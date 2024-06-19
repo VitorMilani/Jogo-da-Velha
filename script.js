64e833d8695a0f3ae7b95929fd992fe751d617f1
guilhermehenriqueSFC <165093826+guilhermehenriqueSFC@users.noreply.github.com>
2024-06-19 18:37:56 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const jogador1Input = document.getElementById('Jogador1');
+const jogador2Input = document.getElementById('Jogador2');
+const botoes = document.querySelectorAll('.base button');
+const mensagem = document.getElementById('mensagem');
+const reiniciarBotao = document.getElementById('reiniciar');
+const tabelaPontuacao = document.getElementById('tabela-pontuacao').getElementsByTagName('tbody')[0];
+const modoJogoSelect = document.getElementById('modo');
+const modoJogoContainer = document.querySelector('.modo-jogo');
+const nomesJogadoresContainer = document.querySelector('.nomes-jogadores');
